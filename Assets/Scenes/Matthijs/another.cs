b005e5b31ce59bbe1d06c0abb5ea24e1f004f73a
LuigiOvertoom <firedream2236@gmail.com>
2018-09-26 11:07:53 +0200

    mappen sturct aangepast

--- Assets/Scenes/Matthijs/another.cs
+++ Assets/Scenes/Matthijs/another.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ookeenmockup : MonoBehaviour {
+public class another : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
